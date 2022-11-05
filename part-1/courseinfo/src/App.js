c4315edc1548369eb36f9f92aea662394c53a3ef
houssamelm <houssamelmasoudi@gmail.com>
2022-11-05 20:53:59 +0100

    1.5

--- part-1/courseinfo/src/App.js
+++ part-1/courseinfo/src/App.js
@@ -17,6 +17,14 @@ const App = () => {
     ]
   }
 
+  // const arto = {
+  //   name: 'Arto Hellas',
+  //   greet: function() {
+  //     console.log('hello, my name is ' + this.name)
+  //   },
+  // }
+  // setTimeout(arto.greet.bind(arto), 1000)
+
   const Header = (name) => {
    
     return <h1>{course.name}</h1>
